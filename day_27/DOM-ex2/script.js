fc695125dc6ca8eae6e98e1dc229bf7f902a5223
ktorres63 <ktorresaro@unsa.edu.pe>
2024-04-04 22:34:16 -0500

    Finish exercise

--- day_27/DOM-ex2/script.js
+++ day_27/DOM-ex2/script.js
@@ -1,17 +1,25 @@
 const ulElem = document.querySelector("ul");
 const input = document.querySelector("input");
 const btn = document.querySelector("button");
-btn.addEventListener("click", response);
+btn.addEventListener("click", () => {
+  let inputText = input.value;
+  input.value = "";
+
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const btnDel = document.createElement("button");
+
+  li.appendChild(span);
+  li.textContent = inputText;
+  li.appendChild(btnDel);
+  btnDel.textContent = "Delete";
+  ulElem.appendChild(li);
+
+  btnDel.addEventListener("click", ()=> {
+    ulElem.removeChild(li);
+  });
+  input.focus();
 
 
-const li = document.createElement("li");
-const span = document.createElement("span");
-const btnDel = document.createElement("button");
+});
 
-li.appendChild(span);
-li.appendChild(btnDel);
-
-function response(){
-  let inputResp = input;
-  input.textContent = ""
-}
