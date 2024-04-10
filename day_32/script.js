459d79a7eb3a2a491e79a2640666d79b1e740f65
ktorres63 <ktorresaro@unsa.edu.pe>
2024-04-09 22:52:37 -0500

    Add progress with etch a sketch project

---
+++ day_32/script.js
@@ -0,0 +1,37 @@
+const container = document.querySelector(".container");
+generateGrid(16);
+
+const sizeBtn = document.querySelector("#saveSize");
+sizeBtn.addEventListener("click", () => {
+  const inputSize = document.querySelector("#inputSize");
+  let size = parseInt(inputSize.value);
+  generateGrid(size);
+});
+
+
+function generateGrid(size){
+  deletePixels(container);
+  container.style.width = `${size * 10 + 2}px`; // container width in px 
+  generatePixels(size)
+}
+function generatePixels(size){
+  for (let i = 0; i < size * size; i++) {
+    let pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    mouseover(pixel);
+    container.appendChild(pixel);
+  }
+}
+function deletePixels(container){
+  const oldPixels = document.querySelectorAll(".pixel");
+  oldPixels.forEach((px)=>{
+    container.removeChild(px)
+  });
+
+}
+
+function mouseover(pixel) {
+  pixel.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "black";
+  });
+}
