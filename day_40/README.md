3b2fa659d5bda11a5620118595b02e9b7b207db3
ktorres63 <ktorresaro@unsa.edu.pe>
2024-04-23 19:02:10 -0500

    Finish exercise 12

--- day_40/README.md
+++ day_40/README.md
@@ -23,3 +23,8 @@ The formula for the average is (age1 + age2 + ... + ageN) / N.
 Let arr be an array.
 
 Create a function unique(arr) that should return an array with unique items of arr.
+## Create keyed object from array
+Let’s say we received an array of users in the form {id:..., name:..., age:... }.
+
+Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
+
--- day_40/ex12/script.js
+++ day_40/ex12/script.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+let users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+let usersById = groupById(users);
+
+console.log(usersById)
+/*
+// after the call we should have:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
+function groupById(arr){
+  return arr.reduce((obj,elem) => {
+    obj[elem.id] = elem
+    return obj
+  },{})
+}
